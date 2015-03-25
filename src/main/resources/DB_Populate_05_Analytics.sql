f57d0d0a8a60e3380679cd6012a03cf035b15646
Jeff Risberg <jrisberg@aol.com>
2015-03-24 23:48:39 -0700

    updated analytic models

--- src/main/resources/DB_Populate_05_Analytics.sql
+++ src/main/resources/DB_Populate_05_Analytics.sql
@@ -1,12 +1,12 @@
 -- analytics
 
-INSERT INTO dimensions (id, name, table_name, group_field, label_field, date_created, last_updated)
+INSERT INTO dimensions (id, name, table_name, group_field, category_field, date_created, last_updated)
 values (1, 'Donor', 'users', 'id', 'email', '2015-01-01', '2015-01-01');
 
-INSERT INTO dimensions (id, name, table_name, group_field, label_field, date_created, last_updated)
+INSERT INTO dimensions (id, name, table_name, group_field, category_field, date_created, last_updated)
 values (2, 'Charity', 'charities', 'id', 'title', '2015-01-01', '2015-01-01');
 
-INSERT INTO dimensions (id, name, table_name, group_field, label_field, date_created, last_updated)
+INSERT INTO dimensions (id, name, table_name, group_field, category_field, date_created, last_updated)
 values (3, 'Vendor', 'vendors', 'id', 'name', '2015-01-01', '2015-01-01');
 
 INSERT INTO measures (id, name, expression, date_created, last_updated)
