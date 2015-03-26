f961812fa945003daa1b354e78ac14603d24aa7b
Jeff Risberg <jrisberg@aol.com>
2015-03-26 14:51:59 -0700

    added sample set of giftCertificates

--- src/main/resources/DB_Populate_50_GiftCertificates.sql
+++ src/main/resources/DB_Populate_50_GiftCertificates.sql
@@ -1,10 +1,10 @@
 -- giftcertificates
 
 INSERT into giftcertificates (donor_id, recipient_email, amount, date_created) values (1, 'tom@gmail.com', 56.0, '2015-01-15');
-INSERT into giftcertificates (donor_id, recipient_email, amount, date_created) values (1, 'jack@gmail.com', 46.0, '2015-02-11');
-INSERT into giftcertificates (donor_id, recipient_email, amount, date_created) values (2, 'sally@gmail.com', 72.0, '2015-02-25');
+INSERT into giftcertificates (donor_id, recipient_email, amount, date_created) values (1, 'jack@gmail.com', 46.0, '2015-01-21');
+INSERT into giftcertificates (donor_id, recipient_email, amount, date_created) values (2, 'sally@gmail.com', 72.0, '2015-01-25');
 
-INSERT into giftcertificates (donor_id, recipient_email, amount, date_created) values (1, 'bob@gmail.com', 56.0, '2015-01-15');
+INSERT into giftcertificates (donor_id, recipient_email, amount, date_created) values (1, 'bob@gmail.com', 56.0, '2015-02-05');
 INSERT into giftcertificates (donor_id, recipient_email, amount, date_created) values (2, 'abc@gmail.com', 46.0, '2015-02-11');
 INSERT into giftcertificates (donor_id, recipient_email, amount, date_created) values (3, 'jill@gmail.com', 72.0, '2015-02-25');
 
