15017cbca9a2f5cc7163a3f82655012d146cd101
Jeff Risberg <jrisberg@aol.com>
2015-03-30 12:47:44 -0700

    added list of report types

--- src/main/java/com/incra/models/enums/ReportType.java
+++ src/main/java/com/incra/models/enums/ReportType.java
@@ -1,5 +1,10 @@
 package com.incra.models.enums;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author Jeff Risberg
  * @since 3/23/2015
@@ -12,9 +17,25 @@ public enum ReportType {
 
     ReportType(String name) {
         this.name = name;
+
+        ObjectRepo.shared_objectList.add(this);
+        ObjectRepo.shared_keyToObjectMap.put(this.name(), this);
     }
 
     String getName() {
         return name;
+    }
+
+    static public List<ReportType> selectAll() {
+        return ObjectRepo.shared_objectList;
+    }
+
+    static public ReportType findByKey(String key) {
+        return ObjectRepo.shared_keyToObjectMap.get(key);
+    }
+
+    static private class ObjectRepo {
+        static protected Map<String, ReportType> shared_keyToObjectMap = new HashMap<String, ReportType>();
+        static protected List<ReportType> shared_objectList = new ArrayList<ReportType>();
     }
 }
